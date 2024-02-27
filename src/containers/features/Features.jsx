d1f160e5af017839ed8d3a9b544083ca44ea5ae7
Xeba111 <sebasmena72@yahoo.com>
2024-02-27 11:58:46 -0500

    React demo for building UI

---
+++ src/containers/features/Features.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+
+import "./features.css";
+import { Feature } from "../../components";
+
+// Se mapea la info de las 4 Feature de abajo para reutilizar los componentes
+const featuresData = [
+	{
+		title: "Improving end distrusts instantly ",
+		text: "From they fine john he give of rich he. They age and draw mrs like. Improving end distrusts may instantly was household applauded.",
+	},
+	{
+		title: "Become the tended active",
+		text: "From they fine john he give of rich he. They age and draw mrs like. Improving end distrusts may instantly was household applauded.",
+	},
+	{
+		title: "Message or am nothing",
+		text: "From they fine john he give of rich he. They age and draw mrs like. Improving end distrusts may instantly was household applauded.",
+	},
+	{
+		title: "Really boy law county",
+		text: "From they fine john he give of rich he. They age and draw mrs like. Improving end distrusts may instantly was household applauded.",
+	},
+];
+
+const Features = () => {
+	return (
+		<div className="gpt3__features section__padding" id="features">
+			<div className="gpt3__features-heading">
+				<h1 className="gradient__text">
+					The Future is Now and You Just Need To Realize It. Step into
+					Future Today & Make it Happen.
+				</h1>
+				<p>Request Early Access to Get Started</p>
+			</div>
+			{/* Aquí es donde se mapean los 4 Feature, esto es un bloque dinámico de código */}
+			<div className="gpt3__features-container">
+				{featuresData.map((item, index) => (
+					<Feature
+						title={item.title}
+						text={item.text}
+						key={item.title + index}
+					/>
+				))}
+			</div>
+		</div>
+	);
+};
+
+export default Features;
